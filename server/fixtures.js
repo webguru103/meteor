bf7cbdd6df7b0d9aa954bdfbdb054ed8f35770de
kweem <martin.kwee@gmail.com>
2015-08-13 17:58:22 +0200

    kantoor fixtures toegevoegd

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,1 +1,21 @@
-
+if (Kantoren.find({}).count() < 1) {
+  Kantoren.insert({
+    naam:'Hoofdzetel',
+    adres: {
+      straat : 'Belgielei',
+      nummer: 34,
+      postcode: 2018,
+      gemeente: 'Antwerpen'
+    }
+  });
+  Kantoren.insert({
+      naam:'Meteor BVBA',
+      adres: {
+        straat : 'Meir',
+        nummer: 1,
+        postcode: 2000,
+        gemeente: 'Antwerpen'
+      }
+    }
+  );
+}
